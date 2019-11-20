d2967292d44bd3f1ddbc30950106b0a0f270e893
Jamie Barton <jamie@notrab.dev>
2019-11-20 16:44:19 +0000

    feat: handler for accept/reject

---
+++ api/handlers/accept.js
@@ -0,0 +1,32 @@
+const { graphQLClient } = require('../client');
+
+const mutation = `
+  mutation acceptCFP($id: ID!) {
+    updateTalk(data: {
+      cfpStatus: ACCEPTED
+    }, where: {
+      id: $id
+    }) {
+      id
+      title
+      cfpStatus
+      cfpEmailSent
+      speaker {
+        name
+        email
+      }
+    }
+  }
+`;
+
+module.exports = async (req, res) => {
+  try {
+    const {
+      query: { id },
+    } = req;
+
+    res.status(204).send(await graphQLClient.request(mutation, { id }));
+  } catch ({ status = 500, message }) {
+    res.status(status).json({ status, message });
+  }
+};
