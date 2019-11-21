2ceac734d4df7b4d3fc6ca52119e351804a9c105
Jamie Barton <jamie@notrab.dev>
2019-11-21 15:32:18 +0000

    fix speaker positioning

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,17 +1,18 @@
 import React from 'react';
+// import Image from 'graphcms-image';
 
-function Sponsor() {
+function Sponsor({ name, headline, photo }) {
   return (
-    <div className="w-full md:w-1/3 px-6">
-      <div className="bg-gray-300 p-6 rounded-lg flex items-center justify-center w-full">
-        <div className="bg-gray-900 h-64">Speaker photo</div>
-
-        <div className="text-gray-500 flex items-center justify-between">
-          <h4 className="text-bold text-2xl">Speaker Name</h4>
-          <span className="flex items-center">Social icons</span>
-        </div>
-        <p className="text-gray-500">Speaker headline</p>
+    <div className="w-full md:w-1/4 px-6">
+      <div className="rounded-lg bg-gray-900 h-64 flex items-center justify-center w-full">
+        {/* <Image image={photo} maxWidth={150} /> */}
       </div>
+
+      <div className="text-gray-500 flex items-center justify-between pt-6">
+        <h4 className="text-gray-800 text-bold text-1xl">{name}</h4>
+        <span className="flex items-center">Social icons</span>
+      </div>
+      <p className="text-gray-500">{headline}</p>
     </div>
   );
 }
